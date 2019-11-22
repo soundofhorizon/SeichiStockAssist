0e8f21a8c6b606cddd6a4da6eb24f7a28ae7978a
xSoundofhorizonx <ooshiba0206@gmail.com>
2019-11-22 22:18:27 +0900

    [FIX]M_valueを変化させることで椎名の価値を変化させるようにする。Debugとして!showValue 数字　で倍率を定められるコマンドを実装

--- SeichiStockAssist.py
+++ SeichiStockAssist.py
@@ -146,14 +146,17 @@ async def on_message(message):
             channel = client.get_channel(643461625663193098)
             # sympyを用いてみる。変数を定義したら後は方程式などが作れる。代入も可能。計算してくれるので使いやすいのでは？
             # 策定事項：sympyの方程式を定義する際は「expr_(変数名)」を用いる。変数には_valueを語尾につけてみる。(変更してもよい)
-            SIINA_value = sympy.Symbol('SIINA_value')
-            M_value = sympy.Symbol('M_value')  # Mは〔MAGNIFICATION(倍率)〕の略
-            expr_SIINA = (SIINA_value * M_value) ** 0.7
-            value = int(expr_SIINA.subs([(SIINA_value, 100), (M_value, 1.2)]))  # ここで数値の代入を行う。辞書型が使える。
-            embed = discord.Embed(title='value_view',
-                                  description=f'椎名の現在の価値は{value}です。',
-                                  color=0xadff2f)
-            await channel.send(embed=embed)
+            SIINA_value = sympy.Symbol('SIINA_value') #椎名の絶対的価値を定める
+            #M_value = sympy.Symbol('M_value')  # Mは〔MAGNIFICATION(倍率)〕の略。今回はここに椎名の個数を入れる
+            msg = f'{message.content}'.replace('!showValue ', '')
+            M_value = int(msg)
+            if M_value >= 0:#倍率が-になることは考慮しない
+                expr_SIINA = SIINA_value + (0.1 * M_value + sympy.sin(M_value)*sympy.cos(M_value ** 2)) ** 2
+                value = int(expr_SIINA.subs([(SIINA_value, 100), (M_value, 1.2)]))  # ここで数値の代入を行う。辞書型が使える。
+                embed = discord.Embed(title='value_view',
+                                      description=f'椎名の現在の価値は{value}です。',
+                                      color=0xadff2f)
+                await channel.send(embed=embed)
 
         # メッセージ削除
         if message.content.startswith('!del '):
