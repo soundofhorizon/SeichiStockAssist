1abbfadc4e471fa54a3feffb56688129ed0a6717
xSoundofhorizonx <ooshiba0206@gmail.com>
2020-03-12 12:50:53 +0900

    [FIX]これで動く？

--- cogs/graphCog.py
+++ cogs/graphCog.py
@@ -14,8 +14,10 @@ class GraphCog(commands.Cog):
         x = np.linspace(0, 100, 1000)
         y = (1 / 3) * x
         plt.plot(x, y, color='green')
+        plt.xlabel("xAxis", fontsize=20, fontname='serif')
+        plt.ylabel("yAxis", fontsize=20, fontname='serif')
         plt.savefig('/tmp/graph.png')  # 一時ファイルに相当するので必ずtmpディレクトリに保存する
-        with open('/tmp/graph.png', 'rb') as f:
+        with open('/tmp/graph.png', mode='rb') as f:
             await ctx.send_file(ctx.channel, f)
 
     @Graph.error
